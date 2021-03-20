849e4f2a976e945b799d44f31e5ce0fa34de1dd2
Sergey L <yo-suke@mail.ru>
2021-03-20 11:51:39 +0300

    add assert tests

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,9 +1,5 @@
+import { strict as assert } from 'assert';
 import { capitalize } from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-    throw new Error('Функция работает неверно!');
-  }
-  
-  if (capitalize('') !== '') {
-    throw new Error('Функция работает неверно!');
-  }
+assert.strictEqual(capitalize('hello'), 'Hello');
+assert.strictEqual(capitalize(''), '');
